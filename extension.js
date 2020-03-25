a0c5ca363a95c8fa1cad8ddf000af7a01b831737
kaushiro <malibu_kimee@yahoo.com>
2020-03-25 21:46:18 +0000

    added a method to run all tests and keyboard shortcuts for both commands

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,53 +9,83 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  function moduleNameOfCurrentFile() {
+    const fname = document.fileName;
+    const relativeFname = fname.replace(vscode.workspace.rootPath + "/", "");
+    return relativeFname.replace(/\//g, ".").replace(/\.py$/, "");
+  }
+  function mungeTestPathIntoConfig(testPath) {
+    term.sendText(
+      `sed -i -e "s/^\\( \\+'test_subset': \\)'.*'/\\1'${testPath}'/" configs/test_config.py`
+    );
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.testUnderCursor', function () {
-		const editor = vscode.window.activeTextEditor
-		const document = editor.document
-		const term = vscode.window.activeTerminal
+  let disposable = vscode.commands.registerCommand(
+    "extension.testUnderCursor",
+    function() {
+      const editor = vscode.window.activeTextEditor;
+      const document = editor.document;
+      const term = vscode.window.activeTerminal;
 
-		function searchBackward(regex) {
-			var lineNum = editor.selection.active.line
-			while (lineNum >= 0) {
-				const curLine = document.lineAt(lineNum).text 
-				if (curLine.match(regex)) {
-					return curLine.match(regex)[1]
-				}
-				lineNum--;
-			}
+      function searchBackward(regex) {
+        var lineNum = editor.selection.active.line;
+        while (lineNum >= 0) {
+          const curLine = document.lineAt(lineNum).text;
+          if (curLine.match(regex)) {
+            return curLine.match(regex)[1];
+          }
+          lineNum--;
+        }
 
-			// FIXME: should probably do something better than this
-			throw new Error('Didnt find search regex');
-		}
+        // FIXME: should probably do something better than this
+        throw new Error("Didnt find search regex");
+      }
 
-		function moduleNameOfCurrentFile() {
-			const fname = document.fileName
-			const relativeFname = fname.replace(vscode.workspace.rootPath + '/', '')
-			return relativeFname.replace(/\//g, '.').replace(/\.py$/, '')
-		}
+      // function moduleNameOfCurrentFile() {
+      //   const fname = document.fileName;
+      //   const relativeFname = fname.replace(
+      //     vscode.workspace.rootPath + "/",
+      //     ""
+      //   );
+      //   return relativeFname.replace(/\//g, ".").replace(/\.py$/, "");
+      // }
 
-		function testPathAtCursor() {
-			const moduleName = moduleNameOfCurrentFile()
-			const className = searchBackward(/^\s*class (\w+)/)
-			const funcName = searchBackward(/^\s*def (test_\w+)/)
-			return `${moduleName}.${className}.${funcName}`
-		}
+      function testPathAtCursor() {
+        const moduleName = moduleNameOfCurrentFile();
+        const className = searchBackward(/^\s*class (\w+)/);
+        const funcName = searchBackward(/^\s*def (test_\w+)/);
+        return `${moduleName}.${className}.${funcName}`;
+      }
+      // function mungeTestPathIntoConfig(testPath) {
+      //   term.sendText(
+      //     `sed -i -e "s/^\\( \\+'test_subset': \\)'.*'/\\1'${testPath}'/" configs/test_config.py`
+      //   );
+      // }
 
-		function mungeTestPathIntoConfig(testPath) {
-			term.sendText(
-				`sed -i -e "s/^\\( \\+'test_subset': \\)'.*'/\\1'${testPath}'/" configs/test_config.py`
-			)
-		}
+      mungeTestPathIntoConfig(testPathAtCursor());
+      term.sendText("python3 ./run.py");
+    }
+  );
 
-		mungeTestPathIntoConfig(testPathAtCursor())
-		term.sendText('python3 ./run.py')
-	});
-
-	context.subscriptions.push(disposable)
+  let allTestsDisposable = vscode.commands.registerCommand(
+    "extension.testPageAtCursor",
+    function() {
+      function testPageAtCursor() {
+        return moduleNameOfCurrentFile();
+      }
+      // function mungeTestPathIntoConfig(testPath) {
+      //   term.sendText(
+      //     `sed -i -e "s/^\\( \\+'test_subset': \\)'.*'/\\1'${testPath}'/" configs/test_config.py`
+      //   );
+      // }
+      mungeTestPathIntoConfig(testPageAtCursor());
+      term.sendText("python3 ./run.py");
+    }
+  );
+  context.subscriptions.push(disposable, allTestsDisposable);
 }
 exports.activate = activate;
 
@@ -63,6 +93,6 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
